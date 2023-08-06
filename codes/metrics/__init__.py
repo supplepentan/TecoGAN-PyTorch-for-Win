ccbe074cb7f745b08b5d386f145b0a497513fe88
supplepentan <supplepentan@gmail.com>
2023-08-06 22:09:17 +0900

    init

--- codes/metrics/__init__.py
+++
@@ -1,8 +0,0 @@
-from .metric_calculator import MetricCalculator
-
-
-def create_metric_calculator(opt):
-    if 'metric' in opt and opt['metric']:
-        return MetricCalculator(opt)
-    else:
-        return None
